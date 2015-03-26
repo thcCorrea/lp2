8439e1d340e2f2a46d4c1ffa0a864e4100d753d5
thcCorrea <thccorrea01@gmail.com>
2015-03-26 15:13:44 -0300

    Create Exponencial.java
    postando o Exponencial

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,19 @@
+
+package exponencial;
+
+
+public class Exponencial {
+    
+
+
+    public static void main(String[] args) {
+        System.out.println("# Q C");
+        for(int i=0; i<=10; i++){
+            System.out.println(i + " " + (i*i) + " " + (i*i*i));
+        }
+    }
+}
+    public static void main(String[] args) {
+    }
+    
+}
